0f675cf3ee0394112943de3b190ec6062a196b71
vishnuk-24 <67917338+vishnuk-24@users.noreply.github.com>
2023-08-12 17:06:47 +0000

    set custom responses.

--- module 1/app.js
+++ module 1/app.js
@@ -3,9 +3,23 @@ const http = require('http');
 
 // Add request listener
 const server = http.createServer((req, res) => {
-    console.log(req);
     // process.exist()
-    console.log(req.url, req.method, req.headers)
+    console.log(req.url, req.method, req.headers);
+
+    const url = req.url;
+
+    if (url === '/') {
+        // send custom response
+        // set text/html content type in header
+        res.setHeader('Content-Type', 'text/html');
+
+        // write response
+        res.write('<html>');
+        res.write('<head><title>hi</title></head>')
+        res.write('<body>Hello World</body>')
+        res.write('</html>');
+        return res.end();
+    };
 });
 
 server.listen(port=3000);
