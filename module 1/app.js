9b427405d365b812b6ac526f245622053e3c577c
vishnuk-24 <67917338+vishnuk-24@users.noreply.github.com>
2023-08-12 16:47:40 +0000

    logging request properties.

--- module 1/app.js
+++ module 1/app.js
@@ -4,6 +4,8 @@ const http = require('http');
 // Add request listener
 const server = http.createServer((req, res) => {
     console.log(req);
+    // process.exist()
+    console.log(req.url, req.method, req.headers)
 });
 
 server.listen(port=3000);
