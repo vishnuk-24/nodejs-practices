d6cf3dd9b998ba80aa00f24417db4919c0798c02
vishnuk-24 <67917338+vishnuk-24@users.noreply.github.com>
2023-08-13 15:18:10 +0000

    parsing request.

--- module 1/app.js
+++ module 1/app.js
@@ -1,5 +1,8 @@
 // import http module
 const http = require('http');
+// import file system module
+const fs = require('fs');
+
 
 // Add request listener
 const server = http.createServer((req, res) => {
@@ -7,6 +10,7 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
 
     const url = req.url;
+    const method = req.method
 
     if (url === '/') {
         // send custom response
@@ -20,6 +24,29 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     };
+
+    if (url == '/file' && method === 'POST') {
+        // Get request data
+        const body = []
+
+        // Gets chunk data from request and push to body array
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        // make parsedBody from body array
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+        })
+
+
+        fs.writeFileSync('text.txt', 'Sample');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
 });
 
 server.listen(port=3000);
