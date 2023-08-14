5fedbd2a6cd95602f5f1a32aa3abf991970a31e1
vishnuk-24 <67917338+vishnuk-24@users.noreply.github.com>
2023-08-14 17:14:09 +0000

    completes assignment.

--- module 1/assignment/app.js
+++ module 1/assignment/app.js
@@ -6,16 +6,19 @@ const server = http.createServer((req, res) => {
 
     console.log("server running....")
 
+    // top endpoint for create-user form
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Assignment 1</title></head>');
         res.write(
-          '<body><h1>Good Night</h1></body>'
+          '<body><h1>Hello Guys</h1></body>'
         );
+        res.write('<form action="/create-user" method="POST"><input type="text" name="username" placeholder="username"><button type="submit">Send</button></form>')
         res.write('</html>');
         return res.end();
     };
 
+    // display a user list
     if (url === '/user') {
         res.write('<html>');
         res.write('<head><title>Assignment 1</title></head>');
@@ -25,6 +28,26 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     };
+
+    // Adds new end point. /create-user
+    if (url === '/create-user') {
+      const body = []
+  
+      req.on('data', chunk => {
+        body.push(chunk)
+      })
+  
+      req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString()
+        const username = parsedBody.split('=')[1]
+  
+        console.log(username)
+      })
+  
+      res.statusCode = 302
+      res.setHeader('Location', '/')
+      res.end()
+    }
 });
 
 server.listen(3000);
