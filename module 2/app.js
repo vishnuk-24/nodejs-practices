7ebe85b7e9f9685ef4a4f1b4c5e36fd463f0907d
vishnuk-24 <vishnukpuravoor@gmail.com>
2023-08-15 19:31:41 +0530

    Adds middlewares.

--- module 2/app.js
+++ module 2/app.js
@@ -6,6 +6,16 @@ const express = require('express');
 // create an express app
 const app = express();
 
+// creating middleware
+app.use((req, res, next) => {
+    console.log("Inside the 1st middleware.....");
+    next(); // jumps next middleware
+});
+
+app.use((req, res, next) => {
+    console.log("Inside the 2nd middleware.....");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
