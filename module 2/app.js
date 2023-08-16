c1d0b945f120464c53200967c003cbce9db2b707
vishnuk-24 <vishnukpuravoor@gmail.com>
2023-08-16 16:07:12 +0530

    Adds routes in middleware.

--- module 2/app.js
+++ module 2/app.js
@@ -5,15 +5,16 @@ const express = require('express');
 const app = express();
 
 // creating middleware
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log("Inside the 1st middleware.....");
-    next(); // jumps next middleware
+    // next(); // jumps next middleware
+    res.send('<h1>HHHHHHHHHHHIIIIIIIIIIIIIIIIIII</h2>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log("Inside the 2nd middleware.....");
     // send response from middleware
-    res.send('<h1>This is from middleware</h2>')
+    res.send('<h1>This is from middleware</h2>');
 });
 
 app.listen(3000);
