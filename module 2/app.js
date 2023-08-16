d55a89a39d94aaaabe229d2f839289bbf7198ded
vishnuk-24 <vishnukpuravoor@gmail.com>
2023-08-16 17:16:51 +0530

    Handles unroutes endpoints and send 404.

--- module 2/app.js
+++ module 2/app.js
@@ -13,4 +13,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRouter);
 app.use(shopRouter);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(3000);
