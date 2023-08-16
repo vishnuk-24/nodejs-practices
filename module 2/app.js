d5e903925531a263a176dc99dd96e518b3ccf6ce
vishnuk-24 <vishnukpuravoor@gmail.com>
2023-08-16 17:21:43 +0530

    Filtering router using endpoints.

--- module 2/app.js
+++ module 2/app.js
@@ -10,7 +10,7 @@ const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
--- module 2/routes/admin.js
+++ module 2/routes/admin.js
@@ -5,10 +5,10 @@ const router = express.Router();
 router.get('/add-product', (req, res, next) => {
     console.log("Inside the 1st middleware.....");
     // next(); // jumps next middleware
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
