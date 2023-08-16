e8ada7a7caf450c0509629addb491ccfdb520936
vishnuk-24 <vishnukpuravoor@gmail.com>
2023-08-16 16:50:18 +0530

    install body-parser and parse the request.

--- module 2/app.js
+++ module 2/app.js
@@ -1,14 +1,22 @@
 // import express packages
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // create an express app
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 // creating middleware
 app.use('/add-product', (req, res, next) => {
     console.log("Inside the 1st middleware.....");
     // next(); // jumps next middleware
-    res.send('<h1>HHHHHHHHHHHIIIIIIIIIIIIIIIIIII</h2>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
